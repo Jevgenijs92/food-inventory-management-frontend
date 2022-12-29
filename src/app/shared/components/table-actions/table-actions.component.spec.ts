34364b9bc620599b8da212810ee7e62289fa9f50
Jevgenijs Jaunslavietis <jevgenijs.jaunslavietis@gmail.com>
2022-12-29 23:20:48 +0100

    Added code coverage to table actions component

--- src/app/shared/components/table-actions/table-actions.component.spec.ts
+++ src/app/shared/components/table-actions/table-actions.component.spec.ts
@@ -1,12 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { TableActionsComponent } from './table-actions.component';
 import { MatIconModule } from '@angular/material/icon';
-import { MockRender } from 'ng-mocks';
+import { MockedComponentFixture, MockRender } from 'ng-mocks';
+import { By } from '@angular/platform-browser';
 
 describe('TableActionsComponent', () => {
   let component: TableActionsComponent;
-  let fixture: ComponentFixture<TableActionsComponent>;
+  let fixture: MockedComponentFixture<
+    TableActionsComponent,
+    TableActionsComponent
+  >;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,7 +22,38 @@ describe('TableActionsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('should emit update event when update button is clicked', () => {
+    const spy = jest.spyOn(fixture.point.componentInstance, 'onUpdate');
+
+    fixture.debugElement
+      .query(By.css('.table-actions__update'))
+      .nativeElement.click();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('should emit delete event when delete button is clicked', () => {
+    const spy = jest.spyOn(fixture.point.componentInstance, 'onDelete');
+
+    fixture.debugElement
+      .query(By.css('.table-actions__delete'))
+      .nativeElement.click();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('should disable delete button when delete button is clicked', () => {
+    const deleteButton = fixture.debugElement.query(
+      By.css('.table-actions__delete')
+    ).nativeElement as HTMLButtonElement;
+
+    deleteButton.click();
+    fixture.detectChanges();
+    expect(fixture.point.componentInstance.deleteClicked).toBeTruthy();
+    expect(deleteButton.disabled).toBeTruthy();
   });
 });
